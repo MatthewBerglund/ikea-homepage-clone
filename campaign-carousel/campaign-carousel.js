c913b117595788a6d701d4747acc33783bb74195
Matthew Berglund <bergitastrophy@gmail.com>
2022-02-08 06:19:53 +0100

    Create campaignCarousel object to contain all functionality of campaign carousel

--- campaign-carousel/campaign-carousel.js
+++ campaign-carousel/campaign-carousel.js
@@ -1,73 +1,68 @@
-// const container = document.querySelector('.campaign-carousel');
-// const leftBtn = document.querySelector('.campaign-carousel__scroll-btn--left');
-// const rightBtn = document.querySelector('.campaign-carousel__scroll-btn--right');
-// const firstCarouselItem = document.querySelector('.item:first-of-type');
-// const lastCarouselItem = document.querySelector('.item:last-of-type');
+const campaignCarousel = {
+  container: document.querySelector('.campaign-carousel'),
+  leftBtn: document.querySelector('.campaign-carousel__scroll-btn--left'),
+  rightBtn: document.querySelector('.campaign-carousel__scroll-btn--right'),
+  firstCarouselItem: document.querySelector('.campaign-item:first-of-type'),
+  lastCarouselItem: document.querySelector('.campaign-item:last-of-type'),
+  bindEvents: function() {
+    this.container.addEventListener('mouseenter', this.handleContainerMouseEnter.bind(this));
+    this.container.addEventListener('mouseleave', this.handleContainerMouseLeave.bind(this));
+    this.leftBtn.addEventListener('click', this.scrollLeft.bind(this));
+    this.rightBtn.addEventListener('click', this.scrollRight.bind(this));
 
-// const scrollWidth = container.scrollWidth;
-// bindEvents();
+    const observer = new IntersectionObserver(this.handleIntersection.bind(this), {
+      root: this.container,
+      threshold: 1.0
+    });
 
-// function bindEvents() {
-//   container.addEventListener('mouseenter', handleContainerMouseEnter);
-//   container.addEventListener('mouseleave', handleContainerMouseLeave);
-//   leftBtn.addEventListener('click', scrollLeft);
-//   rightBtn.addEventListener('click', scrollRight);
+    observer.observe(this.firstCarouselItem);
+    observer.observe(this.lastCarouselItem);
+  },
+  handleContainerMouseEnter: function() {
+    this.leftBtn.classList.add('campaign-carousel__scroll-btn--visible');
+    this.rightBtn.classList.add('campaign-carousel__scroll-btn--visible');
+  },
+  handleContainerMouseLeave: function() {
+    this.leftBtn.classList.remove('campaign-carousel__scroll-btn--visible');
+    this.rightBtn.classList.remove('campaign-carousel__scroll-btn--visible');
+  },
+  handleIntersection: function (entries) {
+    entries.forEach(entry => {
+      if (entry.target === this.firstCarouselItem) {
+        if (entry.isIntersecting) {
+          this.leftBtn.style.display = 'none';
+        } else {
+          this.leftBtn.style.display = 'flex';
+        }
+      }
 
-//   const observer = new IntersectionObserver(handleIntersection, {
-//     root: container,
-//     threshold: 1.0
-//   });
+      if (entry.target === this.lastCarouselItem) {
+        if (entry.isIntersecting) {
+          this.rightBtn.style.display = 'none';
+        } else {
+          this.rightBtn.style.display = 'flex';
+        }
+      }
+    });
+  },
+  scrollRight: function() {
+    const options = {
+      top: 0,
+      left: this.container.scrollWidth / 3,
+      behavior: 'smooth'
+    };
 
-//   observer.observe(firstCarouselItem);
-//   observer.observe(lastCarouselItem);
-// }
+    this.container.scrollBy(options);
+  },
+  scrollLeft: function() {
+    const options = {
+      top: 0,
+      left: -(this.container.scrollWidth / 3),
+      behavior: 'smooth'
+    };
 
-// function handleContainerMouseEnter() {
-//   leftBtn.classList.add('campaign-carousel__scroll-btn--visible');
-//   rightBtn.classList.add('campaign-carousel__scroll-btn--visible');
-// }
+    this.container.scrollBy(options);
+  }
+};
 
-// function handleContainerMouseLeave() {
-//   leftBtn.classList.remove('campaign-carousel__scroll-btn--visible');
-//   rightBtn.classList.remove('campaign-carousel__scroll-btn--visible');
-// }
-
-// function handleIntersection(entries) {
-//   entries.forEach(entry => {
-//     if (entry.target === firstCarouselItem) {
-//       if (entry.isIntersecting) {
-//         leftBtn.style.display = 'none';
-//       } else {
-//         leftBtn.style.display = 'flex';
-//       }
-//     }
-
-//     if (entry.target === lastCarouselItem) {
-//       if (entry.isIntersecting) {
-//         rightBtn.style.display = 'none';
-//       } else {
-//         rightBtn.style.display = 'flex';
-//       }
-//     }
-//   });
-// }
-
-// function scrollRight() {
-//   const options = {
-//     top: 0,
-//     left: scrollWidth / 3,
-//     behavior: 'smooth'
-//   };
-
-//   container.scrollBy(options);
-// }
-
-// function scrollLeft() {
-//   const options = {
-//     top: 0,
-//     left: -(scrollWidth / 3),
-//     behavior: 'smooth'
-//   };
-
-//   container.scrollBy(options);
-// }
+campaignCarousel.bindEvents();
