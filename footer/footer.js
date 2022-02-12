78705b878560d90faad70dd77a32b840a5f49742
Matthew Berglund <bergitastrophy@gmail.com>
2022-02-12 15:08:17 +0100

    Refactor

--- footer/footer.js
+++ footer/footer.js
@@ -1,32 +1,34 @@
-const serviceNavBtn = document.querySelector('#service-nav-top');
-const serviceLinkContainer = document.querySelector('#service-nav-bottom');
-const serviceLinks = document.querySelector('#service-links');
-const serviceMenuIcon = document.querySelector('#service-menu-icon');
+const serviceNav = {
+  clickableArea: document.querySelector('#service-nav-top'),
+  linkContainer: document.querySelector('#service-nav-bottom'),
+  links: document.querySelector('#service-links'),
+  icon: document.querySelector('#service-menu-icon')
+};
 
-serviceNavBtn.addEventListener('click', () => {
-  serviceLinkContainer.classList.toggle('list-nav__bottom--closed');
-  serviceLinks.classList.toggle('list-nav__list--hidden');
-  serviceMenuIcon.classList.toggle('list-nav__icon--reversed');
-});
+const supportNav = {
+  clickableArea: document.querySelector('#support-nav-top'),
+  linkContainer: document.querySelector('#support-nav-bottom'),
+  links: document.querySelector('#support-links'),
+  icon: document.querySelector('#support-menu-icon')
+};
 
-const supportNavBtn = document.querySelector('#support-nav-top');
-const supportLinkContainer = document.querySelector('#support-nav-bottom');
-const supportLinks = document.querySelector('#support-links');
-const supportMenuIcon = document.querySelector('#support-menu-icon');
+const aboutNav = {
+  clickableArea: document.querySelector('#about-nav-top'),
+  linkContainer: document.querySelector('#about-nav-bottom'),
+  links: document.querySelector('#about-links'),
+  icon: document.querySelector('#about-menu-icon')
+};
 
-supportNavBtn.addEventListener('click', () => {
-  supportLinkContainer.classList.toggle('list-nav__bottom--closed');
-  supportLinks.classList.toggle('list-nav__list--hidden');
-  supportMenuIcon.classList.toggle('list-nav__icon--reversed');
-});
+bindEvents();
 
-const aboutNavBtn = document.querySelector('#about-nav-top');
-const aboutLinkContainer = document.querySelector('#about-nav-bottom');
-const aboutLinks = document.querySelector('#about-links');
-const aboutMenuIcon = document.querySelector('#about-menu-icon');
+function bindEvents() {
+  serviceNav.clickableArea.addEventListener('click', toggleClosed.bind(serviceNav));
+  supportNav.clickableArea.addEventListener('click', toggleClosed.bind(supportNav));
+  aboutNav.clickableArea.addEventListener('click', toggleClosed.bind(aboutNav));
+}
 
-aboutNavBtn.addEventListener('click', () => {
-  aboutLinkContainer.classList.toggle('list-nav__bottom--closed');
-  aboutLinks.classList.toggle('list-nav__list--hidden');
-  aboutMenuIcon.classList.toggle('list-nav__icon--reversed');
-});
+function toggleClosed() {
+  this.linkContainer.classList.toggle('list-nav__bottom--closed');
+  this.links.classList.toggle('list-nav__list--hidden');
+  this.icon.classList.toggle('list-nav__icon--reversed');
+}
